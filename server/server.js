229dd4dd88b65999f83860b1e86b7f38b9d2fdf1
Dhruv Dhari <dhruvdhari2003@gmail.com>
2024-11-18 22:15:44 +0530

    finally deployed i guess 2

--- server/server.js
+++ server/server.js
@@ -16,7 +16,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://ecommercewebsite-frontend-6p6p.onrender.com', // Your frontend domain here
+  })
+);
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
